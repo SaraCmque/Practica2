9ef5d3708d02bdfab0871ccc8d0d252b7b249a25
SaraCmque <84357190+SaraCmque@users.noreply.github.com>
2022-11-02 11:08:21 -0500

    Add files via upload

---
+++ Practica2/parte1y2.hs
@@ -0,0 +1,22 @@
+module Parte1y22(solveRPN, condNum) where
+import Data.List
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "+" = (x + y):ys
+          foldingFunction (x:y:ys) "-" = (y - x):ys
+          foldingFunction (x:y:ys) "*" = (x * y):ys
+          foldingFunction (x:y:ys) "/" = (y / x):ys
+          foldingFunction (x:xs) "neg1" = (negate x):xs
+          foldingFunction (x:xs) "raiz2" = (sqrt x):xs
+          foldingFunction (x:xs) "condnumero" = (condNum x):xs
+          foldingFunction xs "sum" = [sum xs]
+          foldingFunction xs "product" = [product (xs)]
+          foldingFunction xs "promedio" = (sum xs / fromIntegral (length xs)):xs
+          foldingFunction xs numberString = read numberString:xs
+
+condNum :: (RealFloat val) => val -> Float
+condNum x
+    | x == 3 = 100
+    | x == 5 = 25
+    | otherwise = 0
